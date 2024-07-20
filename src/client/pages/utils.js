76a5b09d797346e80fd3b75db17fb35c276754f5
Bobby Caya <bob.caya1@gmail.com>
2024-07-20 11:38:13 -0600

    changing url once again

--- src/client/pages/utils.js
+++ src/client/pages/utils.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-const baseUrl = process.env.VITE.REACT_APP_API_URL || 'http://localhost:3000';
+const baseUrl = process.env.VITE.REACT_APP_API_URL;
 export const fetchFromDiscogs = async (endpoint, params) => {
   try {
       const response = await axios.get(`${baseUrl}/api/records`, {
@@ -28,7 +28,7 @@ export async function fetchReleaseData(endpoint ="") {
         const response = await axios.get('http://localhost:3000/api/lastfm')
         return response
     }catch(error){
-        console.error("error fetching LastFM data", error);
+        console.error("error fetching LastFM data", error); 
         throw error;
 
     }
