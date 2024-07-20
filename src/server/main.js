9ad428a1300d3ea22538494ce2469eb3b45ab8da
Bobby Caya <bob.caya1@gmail.com>
2024-07-20 13:11:14 -0600

    change

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ViteExpress from "vite-express";
 import axios from "axios";
-import { env } from "node:process";
+
 
 const app = express();
 const {
@@ -11,8 +11,8 @@ const headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   'Cross-Origin-Resource-Policy': 'cross-origin',
   'User-Agent': 'PostmanDiscogs/1.0',
-  'Authorization': `OAuth oauth_consumer_key="${process.VITE_API_KEY}",oauth_token="${process.env.API_TOKEN}",oauth_signature_method="PLAINTEXT",oauth_timestamp="${Math.floor(Date.now() / 1000)}",oauth_nonce="${randomBytes(16).toString('hex')}",oauth_version="1.0",oauth_signature="${process.env.VITE_OAUTH_SIGNATURE}"`
-};env
+  'Authorization': `OAuth oauth_consumer_key="${process.env.VITE_API_KEY}",oauth_token="${process.env.API_TOKEN}",oauth_signature_method="PLAINTEXT",oauth_timestamp="${Math.floor(Date.now() / 1000)}",oauth_nonce="${randomBytes(16).toString('hex')}",oauth_version="1.0",oauth_signature="${process.env.VITE_OAUTH_SIGNATURE}"`
+};
 
 app.get('/api/records', async (req, res) => {
   try {
