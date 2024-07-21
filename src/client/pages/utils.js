fe75c9e1f4a0cf722988756269a7547a92284a48
Bobby Caya <bob.caya1@gmail.com>
2024-07-21 17:41:03 -0600

    fix fetch

--- src/client/pages/utils.js
+++ src/client/pages/utils.js
@@ -3,23 +3,18 @@ import axios from "axios";
 
 const baseUrl = import.meta.env.VITE_REACT_APP_API_URL
 export const fetchFromDiscogs = async (endpoint, params) => {
-    const query = new URLSearchParams(params).toString();
-    const url = `${baseUrl}/records?${query}`;
-  
-    try {
-      const response = await fetch(url, {
-        method: 'GET'
+    
+  try {
+      const response = await axios.get(`${baseUrl}/records`, {
+          params: params,
       });
-      if (!response.ok) {
-        throw new Error(`Error fetching data from Discogs API: ${response.statusText}`);
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
+      return response;
+  } catch (error) {
       console.error("Error fetching data from Discogs API:", error);
       throw error;
-    }
-  };
+  }
+};
+
 export async function fetchReleaseData(endpoint ="") {
     try {
         const response = await axios.get(`${baseUrl}/api/records/${endpoint}`);
