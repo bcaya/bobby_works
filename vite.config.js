04f0ee8a6184891b49d29b7a322eb1c78c8c4fa1
Bobby Caya <bob.caya1@gmail.com>
2024-07-21 16:06:22 -0600

    config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({ plugins: [react()],
-  
+  define: {
+    'process.env': {
+
+      API_KEY: process.env.API_KEY,
+      API_TOKEN: process.env.API_TOKEN,
+      OAUTH_SIGNATURE: process.env.OAUTH_SIGNATURE
+    }}
 });
